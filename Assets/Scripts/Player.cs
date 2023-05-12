b35539006e99611224d5fdb9a5128b8a62e5e18d
PHoMi126 <caidinhmenh2612@gmail.com>
2023-05-12 18:26:47 +0700

    Restart

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
-    public float moveSpeed;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private Rigidbody MyRigidbody;
+    [SerializeField] private GameObject brick;
     private Vector3 targetPos;
-    public Rigidbody MyRigidbody;
     Vector3 des;
+    RaycastHit oneHit;
     void Start()
     {
         targetPos = transform.position;
+        Invoke("Restart", 6);
     }
 
     public void Move(Vector3 moveDirection)
@@ -20,13 +24,13 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        Debug.DrawLine(transform.position, des, Color.red);
+        //Debug.DrawLine(transform.position, des, Color.red);
         transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
     }
 
     public void FindTheDestination(Vector3 direction)
     {
-        RaycastHit oneHit;
+        
         //hits = Physics.RaycastAll(transform.position, direction, 100f);
         //des = this.transform.position + (direction * 100f);
         Vector3 startRayPos = this.transform.position;
@@ -37,8 +41,10 @@ public class Player : MonoBehaviour
             if(oneHit.collider.tag == "Brick")
             {
                 listBricks.Add(oneHit.collider);
-                startRayPos = new Vector3(oneHit.transform.position.x, startRayPos.y, oneHit.transform.position.z) ;
-                
+                startRayPos = new Vector3(oneHit.transform.position.x, startRayPos.y, oneHit.transform.position.z);
+                GameObject duplicate = Instantiate(brick);
+                brick.SetActive(true);
+
                 Debug.Log("Wall's hit: " + oneHit.transform.name);
             }
             else
@@ -50,6 +56,13 @@ public class Player : MonoBehaviour
                 }
                 break;
             }
+        }
+    }
+    void Restart()
+    {
+        if(oneHit.collider.tag == "WinPos")
+        {
+            SceneManager.LoadSceneAsync("SampleScene");
         }
     }
 }
